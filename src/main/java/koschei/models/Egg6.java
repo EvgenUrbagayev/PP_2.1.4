491db737d38d5ba7f2ecf9de5fcb8991158599dc
Evgeniy <eurbagayev@mail.com>
2024-12-16 23:31:30 +0300

    Second commit

--- src/main/java/koschei/models/Egg6.java
+++ src/main/java/koschei/models/Egg6.java
@@ -1,12 +1,15 @@
 package koschei.models;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Egg6 {
-    private final Needle7 needle7;
 
-    public Egg6(Needle7 needle7) {
+    private Needle7 needle7;
+
+    @Autowired
+    public void setNeedle7(Needle7 needle7) {
         this.needle7 = needle7;
     }
 
